117806da84243a14f9adef4f877a13588ee54c6b
Avinash <avinashchowdhury81@gmail.com>
2024-07-12 13:16:35 +0530

    lazy routing

--- BrowserRouter/src/App.jsx
+++ BrowserRouter/src/App.jsx
@@ -1,40 +1,37 @@
+import React, { Suspense, lazy, useState } from "react";
 import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
-import { Dashboard } from "./pages/Dashboard";
+
+const Dashboard = lazy(() => import('./pages/Dashboard'));
 import { Landing } from "./pages/Landing";
 
 function App() {
   return (
-    <div>
-      <BrowserRouter>
-        <Appbar />
-        <Routes>
-          <Route  path="/" element={<Landing />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <Appbar />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route
+          path="/dashboard"
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <Dashboard />
+            </Suspense>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 function Appbar() {
-  const navi = useNavigate();
-  return <div>
+  const navigate = useNavigate(); 
+
+  return (
     <div style={{ background: "green" }}>
-      <button
-        onClick={() => {
-          navi("/");
-        }}
-      >
-        Landing
-      </button>
-      <button
-        onClick={() => {
-          navi("/dashboard");
-        }}
-      >
-        dashboard
-      </button>
+      <button onClick={() => navigate("/")}>Landing</button>
+      <button onClick={() => navigate("/dashboard")}>Dashboard</button>
     </div>
-  </div>;
+  );
 }
+
 export default App;
