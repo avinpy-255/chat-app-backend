ab8be750a016e9786208930d609e7dc6bb6f8ba9
Avinash <avinashchowdhury81@gmail.com>
2024-07-12 12:53:31 +0530

    Router

--- BrowserRouter/src/App.jsx
+++ BrowserRouter/src/App.jsx
@@ -1,23 +1,40 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { Dashboard } from './pages/Dashboard'
-import { Landing } from './pages/Landing'
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
+import { Dashboard } from "./pages/Dashboard";
+import { Landing } from "./pages/Landing";
 
 function App() {
- 
-
   return (
-   <div>
-   <div style={{background: 'green'}}>
-    Hi there me Topbaar
-   </div>
-        <BrowserRouter>
-          <Routes>
-            <Route exact path="/" element={<Landing />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-          </Routes>
-    </BrowserRouter>
-   </div>
-  )
+    <div>
+      <BrowserRouter>
+        <Appbar />
+        <Routes>
+          <Route  path="/" element={<Landing />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+function Appbar() {
+  const navi = useNavigate();
+  return <div>
+    <div style={{ background: "green" }}>
+      <button
+        onClick={() => {
+          navi("/");
+        }}
+      >
+        Landing
+      </button>
+      <button
+        onClick={() => {
+          navi("/dashboard");
+        }}
+      >
+        dashboard
+      </button>
+    </div>
+  </div>;
+}
+export default App;
